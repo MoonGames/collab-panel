b4e3cce9e8f0d87af27829766a9e840cabbe9fe3
Martin Indra <aktive@seznam.cz>
2012-10-24 21:04:22 +0200

    Editing script for copyright header.

--- add_copyright_to_all_files.sh
+++ add_copyright_to_all_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #install copyright-header from http://github.com/osterman/copyright-header
-copyright-header --license GPL3 -a ./src --copyright-year 2012 --copyright-holder "Martin Indra <aktive@seznam.cz>" --copyright-software "Collab desktop" --copyright-software-description "Software for shared drawing via internet in real-time" -o ./
+copyright-header --license GPL3 -a ./src --copyright-year 2012 --copyright-holder "Martin Indra <aktive@seznam.cz>" --copyright-software "Collab canvas" --copyright-software-description "Java framework for graphics software enabling offline and shared painting" -o ./
 
